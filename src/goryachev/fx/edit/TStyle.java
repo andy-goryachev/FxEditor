df5084a9ff44c721aabb07e24a8ce44286c81083
Andy Goryachev <andy@goryachev.com>
2017-08-31 17:40:12 -0700

    tstyle

--- src/goryachev/fx/edit/TStyle.java
+++ src/goryachev/fx/edit/TStyle.java
@@ -14,12 +14,12 @@ import javafx.scene.text.Font;
  */
 public class TStyle
 {
-	private static final int BOLD = 0x0001;
-	private static final int ITALIC = 0x0002;
-	private static final int UNDERLINE = 0x0004;
-	private static final int SUPERSCRIPT = 0x0008;
-	private static final int SUBSCRIPT = 0x0010;
-	private static final int STRIKETHROUGH = 0x0020;
+	private static final int BOLD = 1;
+	private static final int ITALIC = 1 << 2;
+	private static final int UNDERLINE = 1 << 3;
+	private static final int SUPERSCRIPT = 1 << 4;
+	private static final int SUBSCRIPT = 1 << 5;
+	private static final int STRIKETHROUGH = 1 << 6;
 	
 	private int flags;
 	private Color bg;
@@ -71,78 +71,6 @@ public class TStyle
 	}
 	
 	
-	public boolean isBold()
-	{
-		return getFlag(BOLD);
-	}
-	
-	
-	public void setBold(boolean on)
-	{
-		setFlag(BOLD, on);
-	}
-	
-	
-	public boolean isItalic()
-	{
-		return getFlag(ITALIC);
-	}
-	
-	
-	public void setItalic(boolean on)
-	{
-		setFlag(ITALIC, on);
-	}
-	
-	
-	public boolean isUnderline()
-	{
-		return getFlag(UNDERLINE);
-	}
-	
-	
-	public void setUnderline(boolean on)
-	{
-		setFlag(UNDERLINE, on);
-	}
-	
-	
-	public boolean isSuperScript()
-	{
-		return getFlag(SUPERSCRIPT);
-	}
-	
-	
-	public void setSuperScript(boolean on)
-	{
-		setFlag(SUPERSCRIPT, on);
-	}
-	
-	
-	public boolean isSubScript()
-	{
-		return getFlag(SUBSCRIPT);
-	}
-	
-	
-	public void setSubScript(boolean on)
-	{
-		setFlag(SUBSCRIPT, on);
-	}
-	
-	
-	public boolean isStrikeThrough()
-	{
-		return getFlag(STRIKETHROUGH);
-	}
-	
-	
-	public void setStrikeThrough(boolean on)
-	{
-		setFlag(STRIKETHROUGH, on);
-	}
-	
-	
 	protected boolean getFlag(int flag)
 	{
 		return ((flags & flag) != 0);
@@ -161,6 +89,120 @@ public class TStyle
 		}
 	}
 	
+
+	public boolean isBold()
+	{
+		return getFlag(BOLD);
+	}
+	
+	
+	public void setBold(boolean on)
+	{
+		setFlag(BOLD, on);
+	}
+	
+	
+	public TStyle bold()
+	{
+		setBold(true);
+		return this;
+	}
+	
+	
+	public boolean isItalic()
+	{
+		return getFlag(ITALIC);
+	}
+	
+	
+	public void setItalic(boolean on)
+	{
+		setFlag(ITALIC, on);
+	}
+	
+	
+	public TStyle italic()
+	{
+		setItalic(true);
+		return this;
+	}
+	
+	
+	public boolean isUnderline()
+	{
+		return getFlag(UNDERLINE);
+	}
+	
+	
+	public void setUnderline(boolean on)
+	{
+		setFlag(UNDERLINE, on);
+	}
+	
+	
+	public TStyle underline()
+	{
+		setUnderline(true);
+		return this;
+	}
+	
+	
+	public boolean isSuperScript()
+	{
+		return getFlag(SUPERSCRIPT);
+	}
+	
+	
+	public void setSuperScript(boolean on)
+	{
+		setFlag(SUPERSCRIPT, on);
+	}
+	
+	
+	public TStyle superScript()
+	{
+		setSuperScript(true);
+		return this;
+	}
+	
+	
+	public boolean isSubScript()
+	{
+		return getFlag(SUBSCRIPT);
+	}
+	
+	
+	public void setSubScript(boolean on)
+	{
+		setFlag(SUBSCRIPT, on);
+	}
+	
+	
+	public TStyle subScript()
+	{
+		setSubScript(true);
+		return this;
+	}
+	
+	
+	public boolean isStrikeThrough()
+	{
+		return getFlag(STRIKETHROUGH);
+	}
+	
+	
+	public void setStrikeThrough(boolean on)
+	{
+		setFlag(STRIKETHROUGH, on);
+	}
+	
+	
+	public TStyle strikeThrough()
+	{
+		setStrikeThrough(true);
+		return this;
+	}
+	
 	
 	public Color getForeground()
 	{
@@ -171,6 +213,13 @@ public class TStyle
 	public void setForeground(Color c)
 	{
 		fg = c;
+	}
+	
+	
+	public TStyle foreground(Color c)
+	{
+		setForeground(c);
+		return this;
 	}
 	
 	
@@ -186,6 +235,13 @@ public class TStyle
 	}
 	
 	
+	public TStyle background(Color c)
+	{
+		setBackground(c);
+		return this;
+	}
+	
+	
 	public String getStyle()
 	{
 		return style;
@@ -195,6 +251,13 @@ public class TStyle
 	public void setStyle(String s)
 	{
 		style = s;
+	}
+	
+	
+	public TStyle style(String s)
+	{
+		setStyle(s);
+		return this;
 	}
 	
 	
@@ -210,6 +273,13 @@ public class TStyle
 	}
 
 	
+	public TStyle font(Font f)
+	{
+		setFont(f);
+		return this;
+	}
+	
+	
 	public Node getNode()
 	{
 		return node;
@@ -219,5 +289,12 @@ public class TStyle
 	public void setNode(Node n)
 	{
 		node = n;
+	}
+	
+	
+	public TStyle node(Node n)
+	{
+		setNode(n);
+		return this;
 	}
 }
