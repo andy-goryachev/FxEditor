8aeb0beb48930a036cef91759911e1c8b5f88ab6
Andy Goryachev <andy@goryachev.com>
2018-03-13 16:22:50 -0700

    fx hacks 9

--- src/goryachev/fx/hacks/FxHacks.java
+++ src/goryachev/fx/hacks/FxHacks.java
@@ -1,8 +1,10 @@
 // Copyright © 2016-2017 Andy Goryachev <andy@goryachev.com>
 package goryachev.fx.hacks;
 import goryachev.fx.edit.CHitInfo;
+import java.util.List;
 import javafx.scene.shape.PathElement;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Window;
 
 
 /**
@@ -26,6 +28,12 @@ public abstract class FxHacks
 	/** returns the text position at the specified local coordinates */
 	public abstract int getTextPos(TextFlow t, double x, double y);
 	
+	/** applies global stylesheet on top of the javafx one */
+	public abstract void applyStyleSheet(String old, String cur);
+	
+	/** returns the list of Windows */
+	public abstract List<Window> getWindows();
+	
 	//
 	
 	private static FxHacks instance;
@@ -40,8 +48,7 @@ public abstract class FxHacks
 	{
 		if(instance == null)
 		{
-			// TODO supply Java9 when it comes
-			instance = new FxHacksJava8();
+			instance = new FxHacksJava9();
 		}
 		return instance;
 	}
